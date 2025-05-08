a9f70881e320352dcd8ae9b6dc569064f88059d3
Butawantemi <japhetbuta@gmail.com>
2025-05-08 20:03:45 +0300

    add api context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,11 @@
-import "./App.css";
-import Header from "./components/header";
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home";
-import AddNewBlog from "./pages/add-blog";
+import './App.css'
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/add-blog" element={<AddNewBlog />} />
-      </Routes>
-    </div>
-  );
+
+  return <div>
+    hey Hi
+  </div>
+    
 }
 
-export default App;
+export default App
